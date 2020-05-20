663653eea23fda9d4e5306bf15907d3a39aa0bb5
Andrew Katcha <andrewk@qualtrics.com>
2020-05-19 22:37:52 -0700

    Revert "another try"
    This reverts commit 500f96c1889c8c917c815cc144d43d1c58e47593.

---
+++ deleteAll.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+
+rm -rf node_modules public src
+rm .gitignore babel.config.js package.json README.md yarn.lock
---
+++ src/components/Actions.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="fixed-bottom">
+        <b-container class="actions-container">
+            <b-row class="actions-row">
+                <b-col class="action-column">
+                    <Action
+                        title="Sailing"
+                        type="sailing"
+                        svgURL="https://image.flaticon.com/icons/svg/464/464687.svg"
+                    />
+                </b-col>
+                <b-col class="action-column">
+                    <Action
+                        title="History"
+                        type="history"
+                        svgURL="https://image.flaticon.com/icons/svg/2224/2224395.svg"
+                    />
+                </b-col>
+                <b-col class="action-column">
+                    <Action
+                        title="Homes"
+                        type="realEstate"
+                        svgURL="https://image.flaticon.com/icons/svg/684/684834.svg"
+                    />
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+<script>
+import Action from "./Actions";
+
+export default {
+    name: "Actions",
+    components: {
+        Action,
+    },
+};
+</script>
+<style scoped>
+.actions-container {
+    display: flex;
+    justify-content: center;
+    padding-top: 591.44px / 1127.34px * 100%;
+    position: relative;
+    bottom: 1rem;
+}
+
+.actions-row {
+    justify-content: center;
+    width: 100vw;
+}
+
+.action-column {
+    padding-left: 1vw;
+    padding-right: 1vw;
+}
+</style>
